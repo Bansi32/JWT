297c6091fb599b622937362df4b0868a7dc981d6
Bansi <jpatel19762001@gmail.com>
2022-06-16 23:22:10 +0530

    csv file upload completed

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const { isAuth } = require("../middlewares/isAuth");
-// const multer = require("multer");
-// const path = require("path");
-// const Product = require("../models/product");
-// const csv = require("csvtojson");
-
-// router.use(express.static(path.resolve(__dirname, "upload")));
+const Product = require("../models/product");
+const csvtojson = require("csvtojson");
 
 router.get("/", isAuth, async (req, res) => {
   try {
@@ -29,50 +25,29 @@ router.get("/:id", isAuth, async (req, res) => {
 });
 
 // Testing
-let data;
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-let uploads = multer({ storage: storage });
-
-router.post("/:id/product", isAuth, uploads.single("csv"), (req, res) => {
-  const { id } = req.params;
-  csv()
-    .fromFile(req.file.path)
-    .then((jsonObj) => {
-      console.log(jsonObj);
-      for (let i = 0; i < jsonObj; i++) {
-        data = parseInt(jsonObj[i].quantity);
-        jsonObj[i].quantity = data;
-        data = parseFloat(jsonObj[i].price);
-        jsonObj[i].price = data;
-      }
-      Product.insertMany({ jsonObj, user: id });
-    });
-});
-
-router.get("/:id/product", isAuth, async (req, res) => {
+router.post("/:id/product", isAuth, (req, res) => {
   try {
-    const product = await Product.find({});
-    res.status(201).json({ products: product });
-  } catch (e) {
-    res.status(400).json({ Errors: "No product found" });
-  }
-});
-
-router.get("/:id/product/:id2", isAuth, async (req, res) => {
-  try {
-    const { id2 } = req.params;
-    const product = await Product.findById({ _id: id2 });
-    res.status(201).json({ product: product });
-  } catch (e) {
-    res.status(400).json({ Errors: "No product found" });
+    const { id } = req.params;
+    csvtojson()
+      .fromFile("./test.csv")
+      .then((csvData) => {
+        console.log(csvData);
+        csvData.forEach((e, index) => {
+          const product = new Product({
+            name: e.name,
+            quantity: e.quantity,
+            price: e.price,
+            user: id,
+          });
+          product.save();
+          res.status(201).json({ Products: product });
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  } catch (error) {
+    console.log(error);
   }
 });
 
@@ -80,8 +55,19 @@ router.get("/:id/product/:id2", isAuth, async (req, res) => {
 router.get("/:id/product", isAuth, async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await Product.findMany({ user: id });
+    const product = await Product.find({ user: id });
     res.status(201).json({ products: product });
+  } catch (e) {
+    res.status(400).json({ Errors: "No product found" });
+  }
+});
+
+//particular product
+router.get("/:id/product/:id2", isAuth, async (req, res) => {
+  try {
+    const { id2 } = req.params;
+    const product = await Product.findById({ _id: id2 });
+    res.status(201).json({ product: product });
   } catch (e) {
     res.status(400).json({ Errors: "No product found" });
   }
